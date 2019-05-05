80fb22e1021e6b29476ef1be1d37f7d2b6209f48
Kevin Dai <daikevin19@yahoo.com>
2019-05-05 01:15:41 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -3,12 +3,12 @@ def reformat_languages(languages)
   
   languages.each do |style, info| # style = :oo; info = {:ruby={}}, etc.
     info.each do |name, attributes| # name = {ruby, java}; attributes = {:type, etc.}
-      if new_hash[name] # if new_hash has the name as a key
-        new_hash[name][:style] << style # :style key is filled in by old
-      else # if new_hash does not have the language as a key
-        new_hash[name] = {}
+      if new_hash[name] # if name key already exists
+        new_hash[name][:style] << style # puts in more styles
+      else # if name key does not exist
+        new_hash[name] = {} # make new key and hash
         new_hash[name][:type] = attributes[:type] # copies type from old hash
-        new_hash[name][:style] = [style] # copies styles over in an array
+        new_hash[name][:style] = [style] # copies style over in an array
       end
     end
   end
